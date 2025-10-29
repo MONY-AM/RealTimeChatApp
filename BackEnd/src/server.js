4c4a4c6b70891f5c3fd70199008c2094d6916e2e
MONY-AM <monyyean6@gmail.com>
2025-10-29 09:18:49 +0700

    ready for production

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,18 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
-process.env.PORT = process.env.PORT || 3000;
+const _dirname = path.resolve();
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/message", messageRoute);
+
+//make ready for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../FrontEnd/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(_dirname, "../FrontEnd", "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/message", messageRoute);
 
 export default app;
