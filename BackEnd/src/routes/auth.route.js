6953056a952f145a5b2b32d3c30e3de565629714
MONY-AM <monyyean6@gmail.com>
2025-11-07 11:59:11 +0700

    login and logout functionality added

--- BackEnd/src/routes/auth.route.js
+++ BackEnd/src/routes/auth.route.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signout, signup } from "../controllers/auth.controller.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/sign-up", signup);
-authRoute.post("/sign-in", (req, res) => {
-  res.send("Log in endpoint");
-});
-authRoute.get("/sign-out", (req, res) => {
-  res.send("Log out endpoint");
-});
+authRoute.post("/sign-in", signin);
+authRoute.post("/sign-out", signout);
 export default authRoute;
